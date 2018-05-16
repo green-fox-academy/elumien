cbe26f0bf480d50c01596219277da46d77647f48
Sára Fellner <sara.fellner@gmail.com>
2018-05-16 19:59:35 +0200

    day-18 #3

--- week-07/day-18/Testing/Count Letters/src/CountLetters.java
+++ week-07/day-18/Testing/Count Letters/src/CountLetters.java
@@ -1,23 +1,49 @@
-import java.util.Dictionary;
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class CountLetters {
+
+    public static void main(String[] args) {
+
+        CountLetters cl = new CountLetters();
+
+        System.out.println(cl.countLetters("makrómikróba").toString());
+    }
 
     //Write a function, that takes a string as an argument and returns a dictionary with all letters in the string as keys, and numbers as values that shows how many occurrences there are.
     //Create a test for that.
 
     public HashMap<Character, Integer> countLetters(String string) {
 
-        
+        HashMap<Character, Integer> dictionary = new HashMap<>();
+
+        string = string.toLowerCase();
+
+        char[] stringCharArray = string.toCharArray();
+
+        Set<Character> characterSet = new HashSet<>();
+
+        for (char c : stringCharArray) {
+
+            characterSet.add(c);
+        }
 
 
+        for (char ditinctCharachter: characterSet) {
 
+            int count = 0;
 
+            for (char Charachter : stringCharArray) {
 
+                if (ditinctCharachter == Charachter) {
+                    count++;
+                }
+            }
 
+            dictionary.put(ditinctCharachter, count);
+        }
 
-        HashMap<Character, Integer> lol = new HashMap<>();
-        return  lol;
+        return dictionary;
     }
 
 }
---
+++ week-07/day-18/Testing/Count Letters/test/CountLettersTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+public class CountLettersTest {
+
+    CountLetters countLetters = new CountLetters();
+
+    @Test
+    public void countLettersTest() {
+
+        String string = "guineapig";
+        HashMap<Character, Integer> expectedDictionary = new HashMap<>();
+        expectedDictionary.put('g', 2);
+        expectedDictionary.put('u', 1);
+        expectedDictionary.put('i', 2);
+        expectedDictionary.put('n', 1);
+        expectedDictionary.put('e', 1);
+        expectedDictionary.put('a', 1);
+        expectedDictionary.put('p', 1);
+
+        assertEquals(countLetters.countLetters(string), expectedDictionary);
+
+    }
+}
