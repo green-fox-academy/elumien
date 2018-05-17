69b0d96dc28d9851a9a514f03f84b0c788257567
Fellner Sára <sara.fellner@loxon.eu>
2018-05-17 17:32:15 +0200

    PLUTO IS A COLD COLD CELESTIAL DWARF!

--- week-02/day-5/Strings and ArrayLists/src/Reverse.java
+++ week-02/day-5/Strings and ArrayLists/src/Reverse.java
@@ -1,5 +1,7 @@
+import static jdk.nashorn.internal.objects.NativeArray.reverse;
+
 public class Reverse {
-    public static void main(String... args) {
+    public static void main(String... args){
         String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
 
         // Create a method that can reverse a String, which is passed as the parameter
@@ -7,22 +9,31 @@ public class Reverse {
         // Try to solve this using charAt() first, and optionally anything else after.
 
 
-        //System.out.println(reverse(reversed));
-
+        System.out.println(reverse(reversed));
+        System.out.println(reverseStringBuilder(reversed));
     }
 
-//    public static String reverse(String text) {
-//        String textCopy = text;
-//
-//        for (int i = 0; i < text.length(); i++) {
-//            text.charAt(i).;
-//                    //= textCopy.charAt(text.length() - 1 - i);
-//            System.out.println(text);
-//
-//
-//        }
-//
-//        return text;
-//
-//    }
+    public static String reverse(String text) {
+
+        String reversedText = "";
+
+        for (int i = text.length()-1; i >= 0; i--) {
+
+            reversedText += text.charAt(i);
+
+        }
+
+        return reversedText;
+    }
+
+
+    public static String reverseStringBuilder(String text) {
+
+        StringBuilder reversedText = new StringBuilder(text).reverse();
+
+        text = reversedText.toString();
+
+        return text;
+    }
+
 }
---
+++ week-02/day-5/Strings and ArrayLists/src/SolarSystem.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class SolarSystem{
+    public static void main(String... args){
+        ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"));
+
+        // Saturn is missing from the planetList
+        // Insert it into the correct position
+        // Create a method called putSaturn() which has list parameter and returns the correct list.
+
+        System.out.println(putSaturn(planetList));
+        // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
+
+        System.out.println(PlutoIsNotAColdColdCelestialDwarf(planetList));
+    }
+
+    private static ArrayList<String> putSaturn(ArrayList<String> planetList) {
+
+        planetList.add("Saturn");
+
+        return planetList;
+    }
+
+    private static ArrayList<String> PlutoIsNotAColdColdCelestialDwarf(ArrayList<String> planetList) {
+
+        planetList.add("Pluto");
+
+        return planetList;
+    }
+
+}
